d0527c3f12d6059f1943475f23ad9f08117b6c68
oodik <99489000+oodik@users.noreply.github.com>
2022-03-11 13:46:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,46 +3,10 @@ const express = require("express");
 const app = express();
 
 const { MongoClient } = require("mongodb");
-
 const uri = process.env.MONGODB_URI;
 
-// use the express-static middleware
-app.use(express.static("public"));
-
-// define the first route
-app.get("/api/movie", async function (req, res) {
-  const client = new MongoClient(uri, { useUnifiedTopology: true });
-  
-  try {
-    await client.connect();
-
-    const database = client.db('sample_mflix');
-    const collection = database.collection('movies');
-
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { genres: "Comedy", poster: { $exists: true } };
-    const cursor = await collection.aggregate([
-      { $match: query },
-      { $sample: { size: 1 } },
-      { $project: 
-        {
-          title: 1,
-          fullplot: 1,
-          poster: 1
-        }
-      }
-    ]);
-
-    const movie = await cursor.next();
-
-    return res.json(movie);
-  } catch(err) {
-    console.log(err);
-  }
-  finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+app.get('/api', (req, res) => {
+    res.send("test data OK");
 });
 
 // start the server listening for requests
