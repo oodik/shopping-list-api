21177856985f4ea4813adc6972a15b39f150390f
oodik <99489000+oodik@users.noreply.github.com>
2022-03-12 09:11:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,22 +5,45 @@ const app = express();
 
 
 const { MongoClient } = require("mongodb");
-const uri = process.env.MONGODB_URI + "/data";
+const uri = process.env.MONGODB_URI;
 
 // use the express-static middleware
 app.use(express.static("public"));
-
 // define the first route
-app.get("/api", async function (req, res) {
+app.get("/api/movie", async function (req, res) {
   const client = new MongoClient(uri, { useUnifiedTopology: true });
-
-
-   await client.connect()
-	  .then(() => console.log('Connected to MongoDB!'))
-	  .catch(error => console.error('Could not connect to MongoDB... ', error));
   
-});
+  try {
+    await client.connect();
 
+    const database = client.db('sample_mflix');
+    const collection = database.collection('movies');
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { genres: "Comedy", poster: { $exists: true } };
+    const cursor = await collection.aggregate([
+      { $match: query },
+      { $sample: { size: 1 } },
+      { $project: 
+        {
+          title: 1,
+          fullplot: 1,
+          poster: 1
+        }
+      }
+    ]);
+
+    const movie = await cursor.next();
+
+    return res.json(movie);
+  } catch(err) {
+    console.log(err);
+  }
+  finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+});
 
 const key = "1234";
 
