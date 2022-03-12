364dc840c56de9622621a615d15055176dd72ff1
oodik <99489000+oodik@users.noreply.github.com>
2022-03-12 09:21:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const app = express();
 
 
 const { MongoClient } = require("mongodb");
+const mongoose = require('mongoose');
 const uri = process.env.MONGODB_URI;
 
-mongodb.connect(uri, { useNewUrlParser: true })
+mongoose.connect(uri, { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB!'))
   .catch(err => console.error('Could not connect to MongoDB... ', err));
 
 
-const listSchema = new mongodb.Schema({
+const listSchema = new mongoose.Schema({
     name: String,
     date: String,
     createdBy: String,
