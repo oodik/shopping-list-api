56a00fcc3e38f93e1aea5d083cd921c619e23608
oodik <99489000+oodik@users.noreply.github.com>
2022-03-12 11:15:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -45,11 +45,11 @@ app.get("/api/shopping-list", (req, res) => {
 
 app.post('/api/shopping-list', (req, res) => {
 
-    const nameP = req.body.name;
+    conosle.log(req.body);
 
       
     const itemPost = new ListMongo({
-      name: nameP,
+      name: "ashrt",
       date: "saff",
       createdBy: "moijoi",
       type: "jidlo"
